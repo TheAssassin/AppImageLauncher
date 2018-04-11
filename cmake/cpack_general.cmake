1e49b36c25be54a14279a9a03c4ea41991bf61c6
TheAssassin <theassassin@assassinate-you.net>
2018-04-11 20:59:55 +0200

    Move CPack source package configuration to separate file

--- cmake/cpack_general.cmake
+++ cmake/cpack_general.cmake
@@ -21,23 +21,3 @@ set(CPACK_COMPONENT_APPIMAGELAUNCHER_PACKAGE_DESCRIPTION "AppImageLauncher")
 
 # find more suitable section for package
 set(CPACK_COMPONENT_APPIMAGELAUNCHER_PACKAGE_SECTION misc)
-
-# ignore files
-# requires four backslashes to escape dots etc., probably because the values are evaluated twice,
-# once by CMake and then by CPack
-set(CPACK_SOURCE_IGNORE_FILES
-    # warning: the following pattern requires at least 6 characters before the "build" part to work as intended
-    "([^t][^r][^a][^v][^i][^s])/.*build.*/"
-    "/.*-prefix/"
-    "\\\\..*AppImage$"
-    "\\\\..*AppImage\\\\..*$"
-    "\\\\..*zs-old.*$"
-    "\\\\..*tar.*$"
-    "\\\\..*deb.*$"
-    "/.git/"
-    "/.idea/"
-)
-
-# source tarball configuration
-set(CPACK_SOURCE_PACKAGE_FILE_NAME appimagelauncher-git${APPIMAGELAUNCHER_GIT_COMMIT})
-set(CPACK_SOURCE_GENERATOR TXZ)
---
+++ cmake/cpack_source.cmake
@@ -0,0 +1,23 @@
+# source package options
+
+# ignore files
+# requires four backslashes to escape dots etc., probably because the values are evaluated twice,
+# once by CMake and then by CPack
+set(CPACK_SOURCE_IGNORE_FILES
+    # warning: the following pattern requires at least 6 characters before the "build" part to work as intended
+    "([^t][^r][^a][^v][^i][^s])/.*build.*/"
+    "/.*-prefix/"
+    "\\\\..*AppImage$"
+    "\\\\..*AppImage\\\\..*$"
+    "\\\\..*zs-old.*$"
+    "\\\\..*tar.*$"
+    "\\\\..*deb.*$"
+    "/.git/"
+    "/.idea/"
+    )
+
+# source tarball filename (without extension)
+set(CPACK_SOURCE_PACKAGE_FILE_NAME appimagelauncher-git${APPIMAGELAUNCHER_GIT_COMMIT})
+
+# build .tar.xz archives to get the maximum amount of compression and produce small tarballs
+set(CPACK_SOURCE_GENERATOR TXZ)
