11c2409a7440171d6490d5a436a7ecfdf298798f
TheAssassin <theassassin@assassinate-you.net>
2018-04-11 19:51:13 +0200

    Fix ignore patterns for source package

--- cmake/cpack_general.cmake
+++ cmake/cpack_general.cmake
@@ -26,15 +26,16 @@ set(CPACK_COMPONENT_APPIMAGELAUNCHER_PACKAGE_SECTION misc)
 # requires four backslashes to escape dots etc., probably because the values are evaluated twice,
 # once by CMake and then by CPack
 set(CPACK_SOURCE_IGNORE_FILES
-    "/.*build.*/"
+    # warning: the following pattern requires at least 6 characters before the "build" part to work as intended
+    "([^t][^r][^a][^v][^i][^s])/.*build.*/"
     "/.*-prefix/"
     "\\\\..*AppImage$"
     "\\\\..*AppImage\\\\..*$"
     "\\\\..*zs-old.*$"
     "\\\\..*tar.*$"
     "\\\\..*deb.*$"
-    ".git"
-    ".idea"
+    "/.git/"
+    "/.idea/"
 )
 
 # source tarball configuration
