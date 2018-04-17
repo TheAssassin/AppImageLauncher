3854b4ea4c0189ee8db51e4238aecd0eca0305a9
TheAssassin <theassassin@assassinate-you.net>
2018-04-17 22:08:48 +0200

    Improve type check

--- src/remove_main.cpp
+++ src/remove_main.cpp
@@ -57,8 +57,8 @@ int main(int argc, char** argv) {
 
     const auto type = appimage_get_type(pathToAppImage.toStdString().c_str(), false);
 
-    if (type <= 0) {
-        QMessageBox::critical(nullptr, "AppImage remove error", "Not an AppImage: " + pathToAppImage);
+    if (type <= 0 || type > 2) {
+        QMessageBox::critical(nullptr, "AppImage remove helper error", "Not an AppImage: " + pathToAppImage);
         return 1;
     }
 
