ef2fa9173b7a1fafc36e9326f9bf388444d070ce
TheAssassin <theassassin@assassinate-you.net>
2018-05-15 18:18:44 +0200

    Don't bundle runtime any more
    Therefore, there's no need to build a runtime without the magic bytes
    any more.

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -9,13 +9,6 @@ install(
     DESTINATION lib/appimagelauncher COMPONENT APPIMAGELAUNCHER
 )
 
-# also, install the runtime, which is needed to run AppImages
-install(
-    FILES ${PROJECT_BINARY_DIR}/lib/AppImageKit/src/runtime
-    PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
-    DESTINATION lib/appimagelauncher COMPONENT APPIMAGELAUNCHER
-)
-
 # TODO: find alternative to the following "workaround" (a pretty dirty hack, actually...)
 # bundle update-binfmts as a fallback for distros which don't have it installed
 find_program(UPDATE_BINFMTS
