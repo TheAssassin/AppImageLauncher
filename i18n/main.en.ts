e01f2542c511e5b1366d649f4dfa85444a26bd4f
TheAssassin <theassassin@assassinate-you.net>
2018-05-18 01:06:49 +0200

    Add en_US translation for main UI

--- i18n/main.en.ts
+++ i18n/main.en.ts
@@ -15,154 +15,154 @@
         <location line="+59"/>
         <location line="+117"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Error</translation>
     </message>
     <message>
         <location line="-258"/>
         <source>AppImageLauncher does not support type %1 AppImages at the moment.</source>
-        <translation type="unfinished"></translation>
+        <translation>AppImageLauncher does not support type %1 AppImages at the moment.</translation>
     </message>
     <message>
         <location line="+10"/>
         <source>Could not make AppImage executable: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Could not make AppImage executable: %1</translation>
     </message>
     <message>
         <location line="+18"/>
         <source>Failed to open AppImage for reading: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to open AppImage for reading: %1</translation>
     </message>
     <message>
         <location line="+12"/>
         <source>Failed to create temporary directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to create temporary directory</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Failed to create temporary copy of type 1 AppImage</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to create temporary copy of type 1 AppImage</translation>
     </message>
     <message>
         <location line="+11"/>
         <source>Failed to open temporary AppImage copy for writing</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to open temporary AppImage copy for writing</translation>
     </message>
     <message>
         <location line="+10"/>
         <location line="+8"/>
         <source>Failed to remove magic bytes from temporary AppImage copy</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to remove magic bytes from temporary AppImage copy</translation>
     </message>
     <message>
         <location line="+33"/>
         <source>execv() failed: %1</source>
         <comment>error message</comment>
-        <translation type="unfinished"></translation>
+        <translation>execv() failed: %1</translation>
     </message>
     <message>
         <location line="+26"/>
         <source>runtime not found: no such file or directory: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>runtime not found: no such file or directory: %1</translation>
     </message>
     <message>
         <location line="+26"/>
         <source>execv() failed: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>execv() failed: %1</translation>
     </message>
     <message>
         <location line="+24"/>
         <source>Usage: %1 [options] &lt;path&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>Usage: %1 [options] &lt;path&gt;</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Desktop integration helper for AppImages, for use by Linux distributions.</source>
-        <translation type="unfinished"></translation>
+        <translation>Desktop integration helper for AppImages, for use by Linux distributions.</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Options:</source>
-        <translation type="unfinished"></translation>
+        <translation>Options:</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Display this help and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Display this help and exit</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Display version and exit</source>
-        <translation type="unfinished"></translation>
+        <translation>Display version and exit</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Arguments:</source>
-        <translation type="unfinished"></translation>
+        <translation>Arguments:</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Path to AppImage (mandatory)</source>
-        <translation type="unfinished"></translation>
+        <translation>Path to AppImage (mandatory)</translation>
     </message>
     <message>
         <location line="+16"/>
         <source>Failed to clean up old desktop files</source>
-        <translation type="unfinished"></translation>
+        <translation>Failed to clean up old desktop files</translation>
     </message>
     <message>
         <location line="+24"/>
         <source>Unknown AppImageLauncher option: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Unknown AppImageLauncher option: %1</translation>
     </message>
     <message>
         <location line="+9"/>
         <source>Error: no such file or directory: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Error: no such file or directory: %1</translation>
     </message>
     <message>
         <location line="+10"/>
         <source>Not an AppImage: %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Not an AppImage: %1</translation>
     </message>
     <message>
         <location line="+41"/>
         <source>Integrating it will move the AppImage into a predefined location, and include it in your application launcher.</source>
-        <translation type="unfinished"></translation>
+        <translation>Integrating it will move the AppImage into a predefined location, and include it in your application launcher.</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>To remove or update the AppImage, please use the context menu of the application icon in your task bar or launcher.</source>
-        <translation type="unfinished"></translation>
+        <translation>To remove or update the AppImage, please use the context menu of the application icon in your task bar or launcher.</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>The directory the integrated AppImages are stored in is currently set to:</source>
-        <translation type="unfinished"></translation>
+        <translation>The directory the integrated AppImages are stored in is currently set to:</translation>
     </message>
     <message>
         <location line="+7"/>
         <source>%1 has not been integrated into your system.</source>
-        <translation type="unfinished"></translation>
+        <translation>%1 has not been integrated into your system.</translation>
     </message>
     <message>
         <location line="+5"/>
         <source>Desktop Integration</source>
-        <translation type="unfinished"></translation>
+        <translation>Desktop Integration</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Integrate and run</source>
-        <translation type="unfinished"></translation>
+        <translation>Integrate and run</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Run once</source>
-        <translation type="unfinished"></translation>
+        <translation>Run once</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Cancel</translation>
     </message>
 </context>
 </TS>
