f09b1b392c8394756e10c89c3f4db80a353ba6df
Weblate Admin <weblate@assassinate-you.net>
2018-05-17 23:33:51 +0000

    Added translation using Weblate (German)

---
+++ i18n/main.de.ts
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/main.cpp" line="+109"/>
+        <location line="+10"/>
+        <location line="+18"/>
+        <location line="+12"/>
+        <location line="+14"/>
+        <location line="+11"/>
+        <location line="+10"/>
+        <location line="+8"/>
+        <location line="+59"/>
+        <location line="+117"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="-258"/>
+        <source>AppImageLauncher does not support type %1 AppImages at the moment.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Could not make AppImage executable: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+18"/>
+        <source>Failed to open AppImage for reading: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+12"/>
+        <source>Failed to create temporary directory</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+14"/>
+        <source>Failed to create temporary copy of type 1 AppImage</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+11"/>
+        <source>Failed to open temporary AppImage copy for writing</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <location line="+8"/>
+        <source>Failed to remove magic bytes from temporary AppImage copy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>execv() failed: %1</source>
+        <comment>error message</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>runtime not found: no such file or directory: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+26"/>
+        <source>execv() failed: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Usage: %1 [options] &lt;path&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Desktop integration helper for AppImages, for use by Linux distributions.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Options:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Display this help and exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Display version and exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+2"/>
+        <source>Arguments:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Path to AppImage (mandatory)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+16"/>
+        <source>Failed to clean up old desktop files</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+24"/>
+        <source>Unknown AppImageLauncher option: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+9"/>
+        <source>Error: no such file or directory: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+10"/>
+        <source>Not an AppImage: %1</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>Integrating it will move the AppImage into a predefined location, and include it in your application launcher.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>To remove or update the AppImage, please use the context menu of the application icon in your task bar or launcher.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>The directory the integrated AppImages are stored in is currently set to:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+7"/>
+        <source>%1 has not been integrated into your system.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+5"/>
+        <source>Desktop Integration</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+4"/>
+        <source>Integrate and run</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+1"/>
+        <source>Run once</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+3"/>
+        <source>Cancel</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
