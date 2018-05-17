a118e295fc1250bfce6f38fd1f9de804ee7946da
TheAssassin <theassassin@assassinate-you.net>
2018-05-17 23:35:47 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: AppImageLauncher/Main UI
    Translate-URL: https://translate.assassinate-you.net/projects/appimagelauncher/main-ui/de/

--- i18n/main.de.ts
+++ i18n/main.de.ts
@@ -15,154 +15,154 @@
         <location line="+59"/>
         <location line="+117"/>
         <source>Error</source>
-        <translation></translation>
+        <translation>Fehler</translation>
     </message>
     <message>
         <location line="-258"/>
         <source>AppImageLauncher does not support type %1 AppImages at the moment.</source>
-        <translation></translation>
+        <translation>AppImageLauncher unterstützt zur Zeit keine AppImages vom Typ %1.</translation>
     </message>
     <message>
         <location line="+10"/>
         <source>Could not make AppImage executable: %1</source>
-        <translation></translation>
+        <translation>AppImage konnte nicht ausführbar gemacht werden: %1</translation>
     </message>
     <message>
         <location line="+18"/>
         <source>Failed to open AppImage for reading: %1</source>
-        <translation></translation>
+        <translation>AppImage konnte nicht lesend geöffnet werden: %1</translation>
     </message>
     <message>
         <location line="+12"/>
         <source>Failed to create temporary directory</source>
-        <translation></translation>
+        <translation>Fehler beim Erstellen des temporären Verzeichnisses</translation>
     </message>
     <message>
         <location line="+14"/>
         <source>Failed to create temporary copy of type 1 AppImage</source>
-        <translation></translation>
+        <translation>Temporäre Kopie des Typ-1-AppImages konnte nicht erstellt werden</translation>
     </message>
     <message>
         <location line="+11"/>
         <source>Failed to open temporary AppImage copy for writing</source>
-        <translation></translation>
+        <translation>AppImage konnte nicht schreibend geöffnet werden: %1</translation>
     </message>
     <message>
         <location line="+10"/>
         <location line="+8"/>
         <source>Failed to remove magic bytes from temporary AppImage copy</source>
-        <translation></translation>
+        <translation>Entfernen der magischen Bytes in der temporären AppImage-Kopie fehlgeschlagen</translation>
     </message>
     <message>
         <location line="+33"/>
         <source>execv() failed: %1</source>
         <comment>error message</comment>
-        <translation></translation>
+        <translation>execv() fehlgeschlagen: %1</translation>
     </message>
     <message>
         <location line="+26"/>
         <source>runtime not found: no such file or directory: %1</source>
-        <translation></translation>
+        <translation>runtime nicht gefunden: Datei oder Verzeichnis nicht vorhanden: %1</translation>
     </message>
     <message>
         <location line="+26"/>
         <source>execv() failed: %1</source>
-        <translation></translation>
+        <translation>execv() fehlgeschlagen: %1</translation>
     </message>
     <message>
         <location line="+24"/>
         <source>Usage: %1 [options] &lt;path&gt;</source>
-        <translation></translation>
+        <translation>Benutzung: %1 [Optionen] &lt;Pfad&gt;</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Desktop integration helper for AppImages, for use by Linux distributions.</source>
-        <translation></translation>
+        <translation>Hilfsprogramm zur Desktop-Integration von AppImages, zur Verwendung in Linux-Distributionen</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Options:</source>
-        <translation></translation>
+        <translation>Optionen:</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Display this help and exit</source>
-        <translation></translation>
+        <translation>Hilfetext anzeigen und beenden</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Display version and exit</source>
-        <translation></translation>
+        <translation>Version anzeigen und beenden</translation>
     </message>
     <message>
         <location line="+2"/>
         <source>Arguments:</source>
-        <translation></translation>
+        <translation>Argumente:</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Path to AppImage (mandatory)</source>
-        <translation></translation>
+        <translation>Pfad zu AppImage (erforderlich)</translation>
     </message>
     <message>
         <location line="+16"/>
         <source>Failed to clean up old desktop files</source>
-        <translation></translation>
+        <translation>Fehler beim Aufräumen der alten Desktop-Dateien</translation>
     </message>
     <message>
         <location line="+24"/>
         <source>Unknown AppImageLauncher option: %1</source>
-        <translation></translation>
+        <translation>Unbekannte AppImageLauncher-Option: %1</translation>
     </message>
     <message>
         <location line="+9"/>
         <source>Error: no such file or directory: %1</source>
-        <translation></translation>
+        <translation>Fehler: Datei oder Verzeichnis nicht gefunden: %1</translation>
     </message>
     <message>
         <location line="+10"/>
         <source>Not an AppImage: %1</source>
-        <translation></translation>
+        <translation>Datei ist kein AppImage: %1</translation>
     </message>
     <message>
         <location line="+41"/>
         <source>Integrating it will move the AppImage into a predefined location, and include it in your application launcher.</source>
-        <translation></translation>
+        <translation>Durch die Integration wird das AppImage in ein vordefiniertes Verzeichnis verschoben, und in das Applikationsmenü aufgenommen.</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>To remove or update the AppImage, please use the context menu of the application icon in your task bar or launcher.</source>
-        <translation></translation>
+        <translation>Um das AppImage zu aktualisieren oder zu entfernen, benutzen Sie bitte das Kontextmenü des Anwendungs-Icons in Ihrer Task-Bar oder im Applikationsmenü.</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>The directory the integrated AppImages are stored in is currently set to:</source>
-        <translation></translation>
+        <translation>Das Verzeichnis, in welchem die integrierten AppImages gespeichert werden, ist derzeit:</translation>
     </message>
     <message>
         <location line="+7"/>
         <source>%1 has not been integrated into your system.</source>
-        <translation></translation>
+        <translation>%1 wurde noch nicht in Ihr System integriert.</translation>
     </message>
     <message>
         <location line="+5"/>
         <source>Desktop Integration</source>
-        <translation></translation>
+        <translation>Desktop-Integration</translation>
     </message>
     <message>
         <location line="+4"/>
         <source>Integrate and run</source>
-        <translation></translation>
+        <translation>Integrieren und ausführen</translation>
     </message>
     <message>
         <location line="+1"/>
         <source>Run once</source>
-        <translation></translation>
+        <translation>Einmalig ausführen</translation>
     </message>
     <message>
         <location line="+3"/>
         <source>Cancel</source>
-        <translation></translation>
+        <translation>Abbruch</translation>
     </message>
 </context>
 </TS>
