372169b8700785dc5844d77bffc1b83fd2d338f9
TheAssassin <theassassin@assassinate-you.net>
2018-07-17 16:55:46 +0200

    Fix Docker command syntax

--- travis/travis-bionic.sh
+++ travis/travis-bionic.sh
@@ -9,4 +9,4 @@ IMAGE=appimagelauncher-build:bionic
 
 docker build -t "$IMAGE" -f Dockerfile.build-bionic .
 
-docker run -E ARCH -e TRAVIS_BUILD_NUMBER --rm -it -v $(readlink -f ..):/ws "$IMAGE" bash -xc "export BIONIC=true && cd /ws && travis/travis-build.sh"
+docker run -e ARCH -e TRAVIS_BUILD_NUMBER --rm -it -v $(readlink -f ..):/ws "$IMAGE" bash -xc "export BIONIC=true && cd /ws && travis/travis-build.sh"
