20f7f4dc8caf3879e431cf57f4ad60c8ceddf222
TheAssassin <theassassin@assassinate-you.net>
2018-07-31 21:15:30 +0200

    Add support for config file
    Fixes #63.

--- src/shared.cpp
+++ src/shared.cpp
@@ -23,6 +23,8 @@ extern "C" {
 #include <QMessageBox>
 #include <QObject>
 #include <QRegularExpression>
+#include <QSettings>
+#include <QStandardPaths>
 
 // local headers
 #include "shared.h"
@@ -78,6 +80,29 @@ bool makeNonExecutable(const QString& path) {
 
 
 QDir integratedAppImagesDestination() {
+    // calculate path to config file
+    const auto configPath = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+    const auto configFilePath = configPath + "/appimagelauncher.cfg";
+
+    // if the file does not exist, we'll just use the standard location
+    // while in theory it would have been possible to just write the default location to the file, if we'd ever change
+    // it again, we'd leave a lot of systems in the old state, and would have to write some complex code to resolve
+    // the situation
+    // therefore, the file is simply created, but left empty intentionally
+    if (!QFileInfo::exists(configFilePath)) {
+        QFile file(configFilePath);
+        file.open(QIODevice::WriteOnly);
+        file.write("[AppImageLauncher]\n"
+                   "# destination = ~/Applications");
+    }
+
+    QSettings config(configFilePath, QSettings::IniFormat);
+
+    static const QString keyName("AppImageLauncher/destination");
+
+    if (config.contains(keyName))
+        return config.value(keyName).toString();
+
     return DEFAULT_INTEGRATION_DESTINATION;
 }
 
