216801ccfc780229273806a5391d74e26b4c9d51
Alexis Lopez Zubieta <azubieta90@gmail.com>
2018-09-09 23:53:13 +0200

    Implement CLI only workflow.

--- src/launcher/Launcher.cpp
+++ src/launcher/Launcher.cpp
@@ -259,3 +259,8 @@ QIcon Launcher::getAppImageIcon() {
     }
     return QIcon();
 }
+
+bool Launcher::isAppImageExecutable() {
+    QFileInfo fileInfo(appImagePath);
+    return fileInfo.isExecutable();
+}
--- src/launcher/Launcher.h
+++ src/launcher/Launcher.h
@@ -39,6 +39,8 @@ public:
 
     bool shouldBeIgnored();
 
+    bool isAppImageExecutable();
+
     void executeAppImage();
 
     void overrideAppImageIntegration();
