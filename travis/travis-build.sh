89c14e9d695ebaa892d5bc992105555cce40e4fa
TheAssassin <theassassin@assassinate-you.net>
2019-06-15 23:20:16 +0200

    Fix variable escaping

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -21,7 +21,7 @@ cleanup () {
 trap cleanup EXIT
 
 # store repo root as variable
-REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+REPO_ROOT=$(readlink -f $(dirname "${BASH_SOURCE[0]}")/..)
 OLD_CWD=$(readlink -f .)
 
 pushd "$BUILD_DIR"
