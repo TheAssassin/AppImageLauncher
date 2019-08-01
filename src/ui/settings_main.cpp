1931b8a79dec95fc5f9a49f86edd5e75d08f3b17
Alexis Lopez Zubieta <contact@azubieta.net>
2019-08-01 16:51:13 -0500

    Add fallback icons

--- src/ui/settings_main.cpp
+++ src/ui/settings_main.cpp
@@ -1,10 +1,19 @@
 // libraries
+#include <QIcon>
+#include <QDebug>
 #include <QApplication>
 
 // local
 #include "settings_dialog.h"
 
 int main(int argc, char** argv) {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+    QIcon::setFallbackThemeName("fallback");
+#else
+    // QIcon::setFallbackThemeName is not available until Qt 5.12
+    QIcon::setThemeName("fallback");
+#endif
+
     QApplication app(argc, argv);
     SettingsDialog dialog;
     dialog.show();
