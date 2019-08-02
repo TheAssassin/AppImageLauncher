a10d23b2ee44cd04ca5843efb02426f174ecc0ab
Alexis Lopez Zubieta <contact@azubieta.net>
2019-08-01 19:46:03 -0500

    Restore usage of QIcon::setFallbackThemeName

--- src/ui/settings_main.cpp
+++ src/ui/settings_main.cpp
@@ -7,10 +7,17 @@
 #include "settings_dialog.h"
 
 int main(int argc, char** argv) {
-    // QIcon::setFallbackThemeName doesn't have effect on gtk styles 
-    QIcon::setThemeName("fallback");
-
     QApplication app(argc, argv);
+
+    // The theme icons used by the application may not be available on all target systems therefore a fallback theme
+    // must be used to ensure that the icons will be always displayed
+#if QT_VERSION >= QT_VERSION_CHECK(5, 12, 0)
+    QIcon::setFallbackThemeName("fallback");
+#else
+    // QIcon::setFallbackThemeName is not available until Qt 5.12
+    QIcon::setThemeName("fallback");
+#endif
+
     SettingsDialog dialog;
     dialog.show();
 
