1ecc4ddf66ec8fc6c842d498da99645f208a854e
Alexis Lopez Zubieta <contact@azubieta.net>
2019-08-02 17:59:43 -0500

    Force Fusion style

--- src/ui/settings_main.cpp
+++ src/ui/settings_main.cpp
@@ -1,11 +1,13 @@
 // libraries
 #include <QApplication>
+#include <QtWidgets/QStyleFactory>
 
 // local
 #include "settings_dialog.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
+    QApplication::setStyle(QStyleFactory::create("Fusion"));
     SettingsDialog dialog;
     dialog.show();
 
