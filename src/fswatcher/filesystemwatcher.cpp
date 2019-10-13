3418cf4a979ea7abc5060605ddf9209943b4f913
TheAssassin <theassassin@assassinate-you.net>
2019-10-13 23:49:18 +0200

    Make file system watcher thread safe
    We're calling methods from multiple threads, so this was long overdue.

--- src/fswatcher/filesystemwatcher.cpp
+++ src/fswatcher/filesystemwatcher.cpp
@@ -6,6 +6,7 @@
 // library includes
 #include <QDebug>
 #include <QDir>
+#include <QMutex>
 #include <QTimer>
 #include <QThread>
 #include <sys/inotify.h>
@@ -37,6 +38,7 @@ public:
 public:
     QDirSet watchedDirectories;
     QTimer eventsLoopTimer;
+    QMutex* mutex;
 
 private:
     int inotifyFd = -1;
@@ -45,6 +47,9 @@ private:
 public:
     // reads events from the inotify fd and emits the correct signals
     std::vector<INotifyEvent> readEventsFromFd() {
+        // we don't want to read events in parallel
+        QMutexLocker lock{mutex};
+
         // read raw bytes into buffer
         // this is necessary, as the inotify_events have dynamic sizes
         static const auto bufSize = 4096;
@@ -86,7 +91,7 @@ public:
         return events;
     }
 
-    PrivateData() : isRunning(false), watchedDirectories() {
+    PrivateData() : isRunning(false), watchedDirectories(), mutex(new QMutex) {
         inotifyFd = inotify_init1(IN_NONBLOCK);
         if (inotifyFd < 0) {
             auto error = errno;
@@ -94,6 +99,7 @@ public:
         }
     };
 
+    // caution: method is not threadsafe!
     bool startWatching(const QDir& directory) {
         static const auto mask = fileChangeEvents | fileRemovalEvents;
 
@@ -119,6 +125,8 @@ public:
     }
 
     bool startWatching() {
+        QMutexLocker lock{mutex};
+
         for (const auto& directory : watchedDirectories) {
             if (!startWatching(directory))
                 return false;
@@ -128,6 +136,8 @@ public:
     }
 
     bool startWatching(const QDirSet& directories) {
+        QMutexLocker lock{mutex};
+
         for (const auto& directory : directories) {
             if (!startWatching(directory)) {
                 return false;
@@ -137,6 +147,7 @@ public:
         return true;
     }
 
+    // caution: method is not threadsafe!
     bool stopWatching(int watchFd) {
         // no matter whether the watch removal succeeds, retrying to remove the watch won't help
         // therefore, we can remove the file descriptor from the map in any case
@@ -154,6 +165,8 @@ public:
     }
 
     bool stopWatching() {
+        QMutexLocker lock{mutex};
+
         while (!watchFdMap.empty()) {
             const auto pair = *(watchFdMap.begin());
             const auto watchFd = pair.first;
@@ -169,6 +182,8 @@ public:
     }
 
     bool stopWatching(const QDirSet& directories) {
+        QMutexLocker lock{mutex};
+
         for (const auto& directory : directories) {
             for (const auto& pair : watchFdMap) {
                 if (pair.second == directory) {
@@ -202,33 +217,51 @@ FileSystemWatcher::FileSystemWatcher(const QDirSet& paths) : FileSystemWatcher()
 }
 
 QDirSet FileSystemWatcher::directories() {
+    QMutexLocker lock{d->mutex};
+
     return d->watchedDirectories;
 }
 
 bool FileSystemWatcher::startWatching() {
-    if (d->isRunning) {
-        qDebug() << "tried to start file system watcher while it's running already";
-        return true;
+    {
+        QMutexLocker lock{d->mutex};
+
+        if (d->isRunning) {
+            qDebug() << "tried to start file system watcher while it's running already";
+            return true;
+        }
     }
 
     auto rv = d->startWatching();
 
-    if (rv)
-        d->isRunning = true;
+    {
+        QMutexLocker lock{d->mutex};
+
+        if (rv)
+            d->isRunning = true;
+    }
 
     return rv;
 }
 
 bool FileSystemWatcher::stopWatching() {
-    if (!d->isRunning) {
-        qDebug() << "tried to stop file system watcher while stopped";
-        return true;
+    {
+        QMutexLocker lock{d->mutex};
+
+        if (!d->isRunning) {
+            qDebug() << "tried to stop file system watcher while stopped";
+            return true;
+        }
     }
 
     const auto rv = d->stopWatching();
 
-    if (rv)
-        d->isRunning = false;
+    {
+        QMutexLocker lock{d->mutex};
+
+        if (rv)
+            d->isRunning = false;
+    }
 
     return rv;
 }
@@ -292,15 +325,17 @@ bool FileSystemWatcher::updateWatchedDirectories(QDirSet watchedDirectories) {
     // to stop watching with a fine granularity, we also need to know which directories have been removed
     QDirSet disappearedDirectories = setDifference(d->watchedDirectories, watchedDirectories);
 
-    emit newDirectoriesToWatch(newDirectories);
+    {
+        QMutexLocker lock{d->mutex};
 
-    // now we can update the internal state
-    d->watchedDirectories = watchedDirectories;
+        // now we can update the internal state
+        d->watchedDirectories = watchedDirectories;
 
-    // if the watching hasn't been started yet, we shouldn't start/stop any watches
-    // unfortunately we need an extra variable to track this...
-    if (!d->isRunning)
-        return true;
+        // if the watching hasn't been started yet, we shouldn't start/stop any watches
+        // unfortunately we need an extra variable to track this...
+        if (!d->isRunning)
+            return true;
+    }
 
     if (!d->stopWatching(disappearedDirectories))
         return false;
