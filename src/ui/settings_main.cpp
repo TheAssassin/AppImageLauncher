5ef723c413e8cdbafc5190ffb096272f6a4d84e8
TheAssassin <theassassin@assassinate-you.net>
2019-11-12 00:22:19 +0100

    Enable translations for settings dialog
    Must've been forgotten during the re-design of the settings app.

--- src/ui/settings_main.cpp
+++ src/ui/settings_main.cpp
@@ -2,10 +2,14 @@
 #include <QApplication>
 
 // local
+#include <translationmanager.h>
 #include "settings_dialog.h"
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
+
+    TranslationManager mgr(app);
+
     SettingsDialog dialog;
     dialog.show();
 
