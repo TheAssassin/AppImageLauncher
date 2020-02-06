b45c2b1679f6da5bbfa56a1e90080f25070e5502
TheAssassin <theassassin@assassinate-you.net>
2020-02-07 00:52:46 +0100

    Cache Docker images immediately

--- travis/travis-docker.sh
+++ travis/travis-docker.sh
@@ -49,18 +49,22 @@ else
     build_script=build-lite.sh
 fi
 
+# push built image as cache for future builds to registry
+# we can do that immediately once the image has been built successfully; if its definition ever changes it will be
+# rebuilt anyway
+# credentials shall only be available on (protected) master branch
+if [[ "$DOCKER_USERNAME" != "" ]]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
+    docker push "$IMAGE"
+fi
+
 DOCKER_OPTS=()
 # fix for https://stackoverflow.com/questions/51195528/rcc-error-in-resource-qrc-cannot-find-file-png
 if [ "$TRAVIS" != "" ]; then
     DOCKER_OPTS+=("--security-opt" "seccomp:unconfined")
 fi
 
+# run build
 docker run -e ARCH -e TRAVIS_BUILD_NUMBER --rm -i "${DOCKER_OPTS[@]}" -v $(readlink -f ..):/ws "$IMAGE" \
     bash -xc "export CI=1 && export DEBIAN_DIST=\"$DOCKER_DIST\" && cd /ws && source travis/$build_script"
 
-# push built image as cache for future builds to registry
-# credentials shall only be available on (protected) master branch
-if [[ "$DOCKER_USERNAME" != "" ]]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
-    docker push "$IMAGE"
-fi
