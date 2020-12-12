1051c5ba1173838e730ee84e5d10375a3527d9ee
TheAssassin <theassassin@assassinate-you.net>
2020-12-12 10:08:01 +0100

    Extract docker build into separate script

---
+++ ci/build-docker-image.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+if [[ "$DIST" == "" ]] || [[ "$ARCH" == "" ]]; then
+    echo "Usage: env ARCH=... DIST=... bash $0"
+    exit 1
+fi
+
+set -x
+set -e
+
+# the other script sources this script, therefore we have to support that use case
+if [[ "$BASH_SOURCE" != "" ]]; then
+    this_dir="$(readlink -f "$(dirname "$BASH_SOURCE")")"
+else
+    this_dir="$(readlink -f "$(dirname "$0")")"
+fi
+
+# needed to keep user ID in and outside Docker in sync to be able to write to workspace directory
+uid="$(id -u)"
+image=quay.io/appimagelauncher/build:"$DIST"-"$ARCH"-uid"$uid"
+dockerfile="$this_dir"/Dockerfile."$DIST"-"$ARCH"
+
+if [[ "$BUILD_LITE" != "" ]]; then
+    image="$image"-lite
+    dockerfile="$dockerfile"-lite
+fi
+
+if [ ! -f "$dockerfile" ]; then
+    echo "Error: $dockerfile could not be found"
+    exit 1
+fi
+
+# speed up build by pulling last built image from quay.io and building the docker file using the old image as a base
+docker pull "$image" || true
+# if the image hasn't changed, this should be a no-op
+docker build --cache-from "$image" -t "$image" -f "$dockerfile" --build-arg UID="$uid" "$this_dir"
+
+# push built image as cache for future builds to registry
+# we can do that immediately once the image has been built successfully; if its definition ever changes it will be
+# rebuilt anyway
+# credentials shall only be available on (protected) master branch
+if [[ "$DOCKER_USERNAME" != "" ]]; then
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
+    docker push "$image"
+fi
--- ci/build-in-docker.sh
+++ ci/build-in-docker.sh
@@ -10,34 +10,8 @@ set -e
 
 cd "$(readlink -f "$(dirname "$0")")"
 
-# needed to keep user ID in and outside Docker in sync to be able to write to workspace directory
-uid="$(id -u)"
-image=quay.io/appimagelauncher/build:"$DIST"-"$ARCH"-uid"$uid"
-dockerfile=Dockerfile."$DIST"-"$ARCH"
-
-if [[ "$BUILD_LITE" != "" ]]; then
-    image="$image"-lite
-    dockerfile="$dockerfile"-lite
-fi
-
-if [ ! -f "$dockerfile" ]; then
-    echo "Error: $dockerfile could not be found"
-    exit 1
-fi
-
-# speed up build by pulling last built image from quay.io and building the docker file using the old image as a base
-docker pull "$image" || true
-# if the image hasn't changed, this should be a no-op
-docker build --cache-from "$image" -t "$image" -f "$dockerfile" --build-arg UID="$uid" .
-
-# push built image as cache for future builds to registry
-# we can do that immediately once the image has been built successfully; if its definition ever changes it will be
-# rebuilt anyway
-# credentials shall only be available on (protected) master branch
-if [[ "$DOCKER_USERNAME" != "" ]]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin quay.io
-    docker push "$image"
-fi
+# sets variables $image, $dockerfile
+source build-docker-image.sh
 
 # figure out which build script to use
 if [[ "$BUILD_LITE" == "" ]]; then
