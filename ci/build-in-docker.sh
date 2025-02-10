51088df43a7c87377ac00ca2f574f1c188b5c943
TheAssassin <theassassin@assassinate-you.net>
2025-02-10 22:23:40 +0100

    Add -t flag to Docker run command if possible

--- ci/build-in-docker.sh
+++ ci/build-in-docker.sh
@@ -29,13 +29,16 @@ else
     echo "Host system does not have enough free memory -> building on regular disk"
 fi
 
+[[ -t 0 ]] && DOCKER_OPTS+=("-t")
+
 # run the build with the current user to
 #   a) make sure root is not required for builds
 #   b) allow the build scripts to "mv" the binaries into the /out directory
 uid="$(id -u)"
 # run build
 docker run -e BUILD_LITE -e DIST -e ARCH -e GITHUB_RUN_NUMBER -e GITHUB_RUN_ID --rm -i --user "$uid" -w /ws -e CI=1 \
-     "${DOCKER_OPTS[@]}" -v "$(readlink -f ..):/ws" \
+     "${DOCKER_OPTS[@]}" \
+     -v "$(readlink -f ..):/ws" \
      "$current_branch_tag" \
      bash ci/build.sh
 
